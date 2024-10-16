0a9a5e60b544e28c80e1ff11df92d45f5521766d
Gfooy <barjonathanarie@gmail.com>
2024-10-16 23:16:31 +0300

    started adding fec, split unit tests from src files

--- fec_handler/src/fec_tests.rs
+++ fec_handler/src/fec_tests.rs
@@ -1,10 +1,6 @@
-use crate::FecConfig;
-
-
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::FecConfig;
 
     fn fec_enc(data: &Vec<u8>, config: &FecConfig) -> Vec<Vec<u8>> {
         let mut retval = Vec::with_capacity(config.source_symbols + config.nb_repair as usize);
@@ -16,22 +12,23 @@ mod tests {
         }
         retval
     }
-    
+
     fn fec_dec(data: Vec<Vec<u8>>, config: &FecConfig) -> Vec<u8> {
         let source_block_length = config.data_length;
         let mut n = 0u32;
         let mut decoder = raptor_code::SourceBlockDecoder::new(config.source_symbols);
-        
+
         while !decoder.fully_specified() {
             decoder.push_encoding_symbol(&data[n as usize], n);
             n += 1;
         }
         decoder.decode(source_block_length).unwrap()
     }
-    
+
     #[test]
     fn it_works() {
-        let og_data = String::from("i like to eat stuff because stuff is yummy and good for me yeet");
+        let og_data =
+            String::from("i like to eat stuff because stuff is yummy and good for me yeet");
         let clone = og_data.clone();
         let raw_data = clone.into_bytes();
         let mut config = FecConfig {
